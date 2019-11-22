425b44a26acb5e835b255e843010d4bad087c62f
ManakaYusuke <Manaka.Yusuke@nims.go.jp>
2019-11-22 10:06:50 +0900

    解析２のパラ平衡用のスクリプト登録

---
+++ seach_of_alloy_for_Ni_using_optimization/phase2/bayes_dakota_para/go_opt.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+atomics=(Cr Mo Nb Ta W Zr)
+
+for item in ${atomics[@]}; do
+    echo "treate about atomic $item"
+    python3.6 -u ~/assets/modules/P67_manaka/thermo-calc-bayes-dakota.py 3 10 $item W000020000000230 13bedfd69583faa62be240fcbcd0c0c0b542bc92e1352070f150f8a309f441ed dev-u-tokyo.mintsys.jp $item 2>&1 | tee results-$item.log
+done
